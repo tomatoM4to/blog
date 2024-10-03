c374d9df953c760f00fcedb6c281daee39721807
tomatoM4to <tomatomato2121@gmail.com>
2024-10-03 22:07:14 +0900

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
             <Dummy />
             <SideMenu />
             <div className="flex-1 flex flex-col items-center">
-                <Link href="/os/1-Memory-overview">os</Link>
+                <Link href="/os/1-Computer-System-Overview">os</Link>
                 <Link href="/network/1-Internet-And-Protocol">network</Link>
                 <Link href="/ai/1-AI-overview">ai</Link>
                 <Link href="/database/1-Database-overview">database</Link>
