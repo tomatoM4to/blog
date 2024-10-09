e0b5de5416f7a7340c2de30da2c52a319a2d772a
tomatoM4to <tomatomato2121@gmail.com>
2024-10-10 00:26:28 +0900

    Refactor lists design

--- app/[subject]/[post]/page.tsx
+++ app/[subject]/[post]/page.tsx
@@ -24,7 +24,7 @@ async function getContent(subject: string, post: string) {
         },
         components: {
             // ...heads,
-            // ...lists,
+            ...lists,
             // ...table,
             ...highlights,
             ...code
